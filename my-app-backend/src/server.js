1a6abe203dcf5ada1a772c58d147a795a396b645
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-03-24 14:42:46 +0200

    did stuff

--- my-app-backend/src/server.js
+++ my-app-backend/src/server.js
@@ -11,16 +11,44 @@ app.get('/contact', function(req, res){
 app.get('/resume', function(req, res){
     res.send('hello, this is resume page');
 });
-app.get('/ab(c)?d', function(req, res){
-    res.send('ab(c)?d');
-});
-app.get('/ab*cd', function(req, res){
-    res.send('ab*cd');
-});
-app.get('/ab*cd', function(req, res){
-    res.send('ab*cd');
+// app.get('/ab(c)?d', function(req, res){
+//     res.send('ab(c)?d');
+// });
+// app.get('/ab*cd', function(req, res){
+//     res.send('ab*cd');
+// });
+// app.get('/ab*cd', function(req, res){
+//     res.send('ab*cd');
+// });
+
+// app.get('/products/:gender/:category:campaing', function(rer, res) {
+//     res.send('You are ordering products for '
+//     + req.params.gender
+//     + ' with the product gategory of '
+//     + req.params.params.category
+//     + ' and you want discount for '
+//     + req.params.campaign
+//     );
+// })
+
+//domain.com/api/game/flappybird/upvote
+//domain.com/api/game/flappybird/downvote
+let score = 0;
+
+app.get('/api/game/:gameName/:vote', function(req, res) {
+    if (req.params.vote === 'upvote') {
+        console.log('upvote req')
+        score++;
+        res.send('you have just upvoted ' + req.params.gameName + ' to ' + score );
+    } else if (req.params.vote === 'downvote') {
+        console.log('downvote req')
+        score = (score === 0) ? score : score--;
+        res.send('you have just downvoted ' + req.params.gameName + ' to ' + score );
+    } else {
+        res.send('Request invalid');
+    }
 });
 
-app.listen(8000, function(){
+app.listen(8000, () => {
     console.log('Server started');
 });
