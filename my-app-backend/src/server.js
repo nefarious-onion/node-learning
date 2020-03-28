e09ce24d335d4e52e1acfa9b4a7e4eef51a698ef
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-03-28 15:28:08 +0200

    add partials header, footer and head, add index.ejs

--- my-app-backend/src/server.js
+++ my-app-backend/src/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 
 const app = express();
+//set the filepath for express to find views-folder
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('*', (req, res) => {
-    res.status(405).end();
+app.get('/', (req, res) => {
+    res.render('pages/index');
 });
+
+// app.get('*', (req, res) => {
+//     res.status(405).end();
+// });
 
 app.listen(8000, () => {
     console.log('Server started');
