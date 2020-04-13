f9a42fa584cb9c6de4b0da0b95aec8f64312bdd5
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-04-13 16:37:24 +0300

    add pro-solution

--- my-app-backend/src/server.js
+++ my-app-backend/src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const http = require('http');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
@@ -13,6 +14,64 @@ app.get('/', (req, res) => {
     res.render('pages/index');
 });
 
+const fetchEventsData = (lang, summary) => {
+    const apiUrl = 'http://open-api.myhelsinki.fi/v1/activities/';
+    const url = (lang !== null) ? apiUrl + '?language_filter=' + lang : apiUrl;
+    console.log(url);
+
+    return new Promise((resolve, reject) => {
+        http.get(url, resp => {
+            let data = '';
+
+            resp.on('data', chunk => {
+                // collect all data to data
+                data += chunk;
+            })
+
+            // parse data when http request completed
+            resp.on('end', () => {
+                console.log('end of data received');
+                // console.log("data", data.toString());
+                // console.log("response", resp);
+                const json = JSON.parse(data.toString());
+                console.log("data", json);
+
+                if (data === '') {
+                    resolve(new Array());
+                } else {
+                    // const events = json
+                    //     .filter(event => )
+                    //     .map(event => );
+                    let final = [];
+                    resp.data.forEach(event => {
+                        if (event.info_url && event.name[lang]) {
+                            final.push({
+                                url: event.info_url,
+                                name: event.name[lang]
+                            })
+                        }
+                    })
+                    // resolve(final);
+                    resolve(events);
+                }
+            })
+        })
+            .on('error', err => {
+                console.log(err)
+                reject(err);
+            });
+    });
+}
+
+app.get('/api/allevents/:lang', (req, res) => {
+    fetchEventsData(req.params.lang)
+        .then(data => {
+            res.send.data;
+        })
+        .catch(err => {
+            res.send(err);
+        });
+});
 
 
 app.get('*', (req, res) => {
